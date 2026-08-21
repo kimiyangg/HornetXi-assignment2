c71efb5a0fc60e1f0b6b664ed7b1890fbfeff6e1
kimi <kimiyang951116@gmail.com>
2026-08-21 16:27:09 +0000

    some basic pid tuning result

--- assignment2/controller.py
+++ assignment2/controller.py
@@ -8,11 +8,11 @@ class PID_Controller(Node):
         super().__init__("pid_controller")
         
         # Depth (Y axis) control variables
-        self.setpoint = 0
+        self.setpoint = -20
         self.depth = 0
         
         # X axis control variables
-        self.setpoint_x = 0
+        self.setpoint_x = 20
         self.x = 0
         
         # Publishers for thrust_depth (vertical) and thrust_x (horizontal)
@@ -32,20 +32,29 @@ class PID_Controller(Node):
                 10)
                 
         #TODO: write subscriptions for 'setpoint_x' and 'x'
-        
+        self.sub_setpoint_x = self.create_subscription(
+                Float64,
+                'setpoint_x',
+                self.setpoint_x_callback,
+                10)
+        self.sub_x = self.create_subscription(
+                Float64,
+                'x',
+                self.x_callback,
+                10)
 
         #TODO: utilise and tune these gains
         # Gains for depth (Y axis)
-        self.KP_depth = 0.0
-        self.KI_depth = 0.0
-        self.KD_depth = 0.0
+        self.KP_depth = 2.0
+        self.KI_depth = 0
+        self.KD_depth = 1.2
         
         # Gains for X axis (horizontal)
-        self.KP_x = 0.0
-        self.KI_x = 0
-        self.KD_x = 0.0
+        self.KP_x = 2.5
+        self.KI_x = 0.0
+        self.KD_x = 1.0
         
-        self.bias = 0.0 # Is a bias necessary?
+        self.bias = -2.0 # Is a bias necessary?
 
         # Variables for integral and derivative error (Y axis)
         self.error_sum_depth = 0
@@ -67,29 +76,43 @@ class PID_Controller(Node):
     
     #TODO: callbacks for 'setpoint_x' and 'x'
 
+    def setpoint_x_callback(self, msg):
+        self.setpoint_x = msg.data
+
+    def x_callback(self, msg):
+        self.x = msg.data
+
     #TODO: complete this function
     def timer_callback(self):
         # PID for depth
+        thrust = Float64()
+        error_depth = self.setpoint - self.depth
+        self.error_sum_depth += error_depth * self.timer_period
+        derivative_depth = (error_depth - self.last_error_depth) / self.timer_period
+        thrust.data = float(self.KP_depth * error_depth + self.KI_depth * self.error_sum_depth + self.KD_depth * derivative_depth) * -1 + self.bias
+        self.last_error_depth = error_depth
 
-        
         # PID for X axis
-
-
+        thrust_x = Float64()
+        error_x = self.setpoint_x - self.x
+        self.error_sum_x += error_x * self.timer_period
+        derivative_x = (error_x - self.last_error_x) / self.timer_period
+        thrust_x.data = float(self.KP_x * error_x + self.KI_x * self.error_sum_x + self.KD_x * derivative_x)
+        self.last_error_x = error_x
 
         # We should set vertical thrust to 0 if moving upwards, letting buoyancy do the work?
 
         # Packaging Data
-        thrust = Float64()
-        thrust.data = 0.0
+        # thrust.data = 0.0
         
-        thrust_x = Float64()
-        thrust_x.data = 0.0
+
+        # thrust_x.data = 0.0
         
         # Publishing Data
         self.pub.publish(thrust)
         self.pub_x.publish(thrust_x)
 
-        # self.get_logger().info(f"Publishing thrust: {thrust.data}, thrust_x: {thrust_x.data}")
+        self.get_logger().info(f"Publishing thrust: {thrust.data}, thrust_x: {thrust_x.data}")
 
 def main(args=None):
     rclpy.init(args=args)
